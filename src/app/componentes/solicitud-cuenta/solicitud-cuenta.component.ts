d039a09c5e07a2a72ad2dbc0f64dc9acaf865ef3
hcslearning <learning@hardcybersoft.cl>
2024-05-08 16:28:53 -0400

    camera ok

--- src/app/componentes/solicitud-cuenta/solicitud-cuenta.component.ts
+++ src/app/componentes/solicitud-cuenta/solicitud-cuenta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Camera, CameraResultType } from '@capacitor/camera'
 import { IonicModule } from '@ionic/angular'
 import { addIcons } from 'ionicons'
 import { idCardOutline, journalOutline } from 'ionicons/icons'
@@ -11,8 +12,10 @@ import { idCardOutline, journalOutline } from 'ionicons/icons'
   imports: [ IonicModule ]
 })
 export class SolicitudCuentaComponent  implements OnInit {
-cedulaFront: string = "/assets/cedula-chilena-front.jpg"
-cedulaBack: string = "/assets/cedula-chilena-back.jpg"
+  cedulaFrontDefault: string = "/assets/cedula-chilena-front.jpg"
+  cedulaBackDefault: string = "/assets/cedula-chilena-back.jpg"
+  cedulaFront: string = this.cedulaFrontDefault
+  cedulaBack: string = this.cedulaBackDefault
 
   constructor() {
     addIcons({
@@ -22,5 +25,23 @@ cedulaBack: string = "/assets/cedula-chilena-back.jpg"
   }
 
   ngOnInit() {}
+
+  async tomarFotoCedulaFrontal() {
+    this.cedulaFront = "data:image/png;base64, "
+    this.cedulaFront += (await this.tomarFoto()).base64String ?? this.cedulaFrontDefault
+  }
+
+  async tomarFotoCedulaTrasera() {
+    this.cedulaBack = "data:image/png;base64, "
+    this.cedulaBack += (await this.tomarFoto()).base64String ?? this.cedulaBackDefault
+  }
+
+  async tomarFoto() {
+    return await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.Base64
+    })
+  }
 
 }
